7d52fab05efcb5bfa8b3e6837ef89f3c53f768a6
Yash Parmar <whooyash@gmail.com>
2025-04-12 13:24:05 +0530

    second

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Yash Kiritkumar Parmar',
+  description: 'Created with a cup of Chai',
+  generator: 'Yash',
 }
 
 export default function RootLayout({
