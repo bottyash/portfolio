c712f364802baef0871664c2875deaf71edee948
bottyash <prmryash2004@gmail.com>
2026-09-21 18:32:05 +0530

    model change due to restrictions on previous

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -47,7 +47,7 @@ export async function POST(req: Request) {
     };
 
     const result = streamText({
-      model: mistral('mistral-large-latest'),
+      model: mistral('ministral-3b-2512'),
       messages,
       toolCallStreaming: true,
       tools,
